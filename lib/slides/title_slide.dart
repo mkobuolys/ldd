7102b68b61e647ade01cb34bd1f064e8897bac05
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2024-06-15 22:16:33 +0300

    Add flutter_deck, Mason brick and title slide

---
+++ lib/slides/title_slide.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+
+class TitleSlide extends FlutterDeckSlideWidget {
+  const TitleSlide()
+      : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            route: '/title',
+            footer: FlutterDeckFooterConfiguration(showFooter: false),
+          ),
+        );
+
+  @override
+  FlutterDeckSlide build(BuildContext context) {
+    return FlutterDeckSlide.title(
+      title: 'Cat-like Development 🐱',
+      subtitle: 'Flutter for Efficiency',
+    );
+  }
+}
